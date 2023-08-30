8ec18d7d1c9a145644a37f2c0d0fcd4aee9204f1
Augustin Laurent <augustin.lrt2@icloud.com>
2023-08-30 23:21:48 +0200

    Suppresion de composants inutilisé: - Card - Dialog - Text-Transition Mise à jour de la fonction deleteUserFromData pour réduire sa complexité algorithmique et ajout d'Error Handler

--- algoRepartition/deleteUserFromData.js
+++ algoRepartition/deleteUserFromData.js
@@ -1,4 +1,33 @@
+// Nouvelle version de la fonction deleteUserFromData avec une complexité en O(n^3) plus faible, cependant elle reste bien trop élevée pour être utilisée dans le code final, lors de la migration de la BD un travail devra être fait dessus
+// TODO : 
+// - Trouver une solution pour réduire la complexité de la fonction deleteUserFromData
+
 export function deleteUserFromData(userList, data) {
+    if (!Array.isArray(userList)) throw new Error('userList devrait être un array');
+    if (typeof data !== 'object' || data === null) throw new Error("Mauvais type d'objet");
+    const updatedData = { ...data };
+    for (const userID of userList) {
+        for (const projectKey in updatedData) {
+            const project = updatedData[projectKey];           
+            for (const choixKey in project) {
+                const choix = project[choixKey];
+                for (const roleKey in choix) {
+                    const role = choix[roleKey];
+                    if (!Array.isArray(role)) throw new Error(`Invalid role data in project: ${projectKey}, choice: ${choixKey}, role: ${roleKey}`);
+                    updatedData[projectKey][choixKey][roleKey] = role.filter(user => user !== userID);
+                }
+            }
+        }
+    }
+
+    return updatedData;
+}
+
+
+
+// Ancienne version de la fonction deleteUserFromData avec une complexité en O(n^4) trop élevée 
+
+/* export function deleteUserFromData(userList, data) {
     var list_choix = ['Choix 1', 'Choix 2', 'Choix 3'];
     for (const userIndex in userList) {
         const userID = userList[userIndex]
@@ -19,4 +48,4 @@ export function deleteUserFromData(userList, data) {
         }
     }
     return data
-}
+} */
