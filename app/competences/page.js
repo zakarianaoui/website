ecb579b1806469c181d61d665074a8406d650ede
Tessa Roche <104842476+trqWork@users.noreply.github.com>
2024-05-28 15:29:11 +0200

    Update page.js

--- app/competences/page.js
+++ app/competences/page.js
@@ -8,15 +8,7 @@ export default function Competences() {
         <Link href={'/'}>
           <ArrowLeftIcon className="absolute w-10 h-10 text-blue-500 top-5 left-10" />
         </Link>
-        <h1 className="my-8 text-5xl font-extrabold text-center md:text-6xl lg:text-7xl">
-          Competency matrix of IDeA
-        </h1>
-        <div className="flex items-center justify-center w-full">
-          <a href="https://app.powerbi.com/reportEmbed?reportId=12f6d628-7428-49f0-9e2c-600c760996b0&autoAuth=true&ctid=35c1785d-3361-4341-a29e-fc685e52b8a5" target="_blank" rel="noopener noreferrer" className="rounded-lg shadow-lg">
-            Go to Power BI Page
-          </a>
-
-        </div>
+       
         <div className="font-extrabold position:relative right:10px">
           <h1 className="my-8 text-center text-5xl font-extrabold md:text-6xl lg:text-7xl">
             Guide d'analyse du dashboard
@@ -58,9 +50,14 @@ export default function Competences() {
                 Les volets de filtres sont liés aux différents onglets.
               </p>
               <br></br>
-              <br></br>
             </ul>
           </h1>
+        </div>
+        <div className="flex items-center justify-center w-full">
+          <a href="https://app.powerbi.com/reportEmbed?reportId=12f6d628-7428-49f0-9e2c-600c760996b0&autoAuth=true&ctid=35c1785d-3361-4341-a29e-fc685e52b8a5" target="_blank" rel="noopener noreferrer" className="rounded-lg shadow-lg">
+            Go to Power BI Page
+          </a>
+
         </div>
       </div>
     </div>
